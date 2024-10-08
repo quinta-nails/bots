b716f94bfb6a56a0c0f0a12c90029d6bb712b0c6
jonkofee <jonkofee@icloud.com>
2024-10-08 12:37:12 +0300

    rm reserve

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,23 +3,18 @@ package main
 import (
 	_ "github.com/amacneil/dbmate/v2/pkg/driver/postgres"
 	"github.com/bufbuild/protovalidate-go"
-	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
-	"github.com/quinta-nails/bots/internal/config"
 	"github.com/quinta-nails/bots/internal/db"
 	"github.com/quinta-nails/bots/internal/server"
 	pb "github.com/quinta-nails/protobuf/gen/go/bots"
-	pbCompanies "github.com/quinta-nails/protobuf/gen/go/companies"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 	"log"
 )
 
 type Service struct {
 	pb.UnimplementedTelegramBotsServiceServer
-	db                     *db.Queries
-	validator              *protovalidate.Validator
-	companiesServiceClient pbCompanies.CompaniesServiceClient
+	db        *db.Queries
+	validator *protovalidate.Validator
 }
 
 func init() {
@@ -35,11 +30,6 @@ func init() {
 }
 
 func main() {
-	servicesConfig := config.ServicesConfig{}
-	if err := env.Parse(&servicesConfig); err != nil {
-		log.Fatal(err)
-	}
-
 	service := &Service{}
 
 	validator, err := protovalidate.New()
@@ -52,15 +42,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	service.db = dbConnection
-
-	companiesServiceConnection, err := grpc.NewClient(servicesConfig.CompaniesEndpoint, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatal("can't connect to companies grpc server")
-	}
-	defer companiesServiceConnection.Close()
-	service.companiesServiceClient = pbCompanies.NewCompaniesServiceClient(companiesServiceConnection)
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterTelegramBotsServiceServer(grpcServer, service)
--- internal/config/services.go
+++
@@ -1,5 +0,0 @@
-package config
-
-type ServicesConfig struct {
-	CompaniesEndpoint string `env:"GRPC_COMPANIES_ENDPOINT,required" envDefault:"127.0.0.1:51052"`
-}
